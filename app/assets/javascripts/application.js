795d874bff5ae1e0693e826fe584eb995e3f632e
Rungroj Kulapan <icaria555@hotmail.com>
2016-11-28 07:19:51 +0000

    update feature add, delete order v.1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,22 +31,47 @@ $(document).ready(function () {
 function addRowOrder() {
     var div = document.createElement('div');
 
-    div.className = 'row';
+    div.className = 'field';
     var productelement = document.getElementById('productlist').lastElementChild
     var generate_num = parseInt(productelement.getAttribute("id").split("_")[1]) + 1
-    var select = productelement.firstElementChild
-    console.log(select.getAttribute("id"))
-
-    select.setAttribute("id", "products_" + generate_num.toString())
-    select = productelement.firstChild
-
+    div.id = 'row_' + generate_num.toString()
+    var select = productelement.firstElementChild.cloneNode(true)
     
-
-    div.parseHTML('<input type="number" name="quantity_1" id="quantity_' + generate_num.toString() + '" />\
-      <input type="number" name="price_1" id="price_' + generate_num.toString() + '" />\
-      <input type="number" name="pv_1" id="pv_' + generate_num.toString() + '" />');
+    select.setAttribute("id", "products_" + generate_num.toString())
     div.appendChild(select)
-
-
+    div.innerHTML = div.innerHTML + '<input type="number" name="quantity_1" id="quantity_' + generate_num.toString() + '" />\
+      <input type="number" name="price_1" id="price_' + generate_num.toString() + '" />\
+      <input type="number" name="pv_1" id="pv_' + generate_num.toString() + '" />\
+      <button name="button" type="button" onclick="javascript:deleteRow(' + generate_num.toString() + ')">delete</button>';
+    //console.log(div.innerHTML)
     document.getElementById('productlist').appendChild(div);
+    //console.log(document.getElementById('productlist').innerHTML);
+}
+
+function addRowStatement() {
+    var div = document.createElement('div');
+
+    div.className = 'field';
+    var productelement = document.getElementById('productlist').lastElementChild
+    var generate_num = parseInt(productelement.getAttribute("id").split("_")[1]) + 1
+    div.id = 'row_' + generate_num.toString()
+    var select = productelement.firstElementChild.cloneNode(true)
+    
+    select.setAttribute("id", "products_" + generate_num.toString())
+    div.appendChild(select)
+    div.innerHTML = div.innerHTML + '<input type="number" name="quantity_1" id="quantity_' + generate_num.toString() + '" />\
+      <input type="number" name="price_1" id="price_' + generate_num.toString() + '" />\
+      <input type="number" name="pv_1" id="pv_' + generate_num.toString() + '" />\
+      <button name="button" type="button" onclick="javascript:deleteRow(' + generate_num.toString() + ')">delete</button>';
+    //console.log(div.innerHTML)
+    document.getElementById('productlist').appendChild(div);
+    //console.log(document.getElementById('productlist').innerHTML);
+}
+
+function deleteRow(row_num) {
+    var id_name = "row_" + row_num
+    var delete_target = document.getElementById(id_name);
+    var parentNode = delete_target.parentNode
+    parentNode.removeChild(delete_target)
+    console.log(row_num)
 }
