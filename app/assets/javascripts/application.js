4cba1290af687f7579beb786dbaf69be6721c9ce
Rungroj Kulapan <icaria555@hotmail.com>
2016-11-30 16:40:12 +0000

    update price pv calculate function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,9 +39,9 @@ function addRowOrder() {
     
     select.setAttribute("id", "products_" + generate_num.toString())
     div.appendChild(select)
-    div.innerHTML = div.innerHTML + '<input type="number" name="quantity_1" id="quantity_' + generate_num.toString() + '" />\
-      <input type="number" name="price_1" id="price_' + generate_num.toString() + '" />\
-      <input type="number" name="pv_1" id="pv_' + generate_num.toString() + '" />\
+    div.innerHTML = div.innerHTML + '<input type="number" name="quantity_1" id="quantity_' + generate_num.toString() + '" value="0" />\
+      <input type="number" name="price_1" id="price_' + generate_num.toString() + '" value="0" />\
+      <input type="number" name="pv_1" id="pv_' + generate_num.toString() + '" value="0" />\
       <button name="button" type="button" onclick="javascript:deleteRow(' + generate_num.toString() + ')">delete</button>';
     //console.log(div.innerHTML)
     document.getElementById('productlist').appendChild(div);
@@ -59,9 +59,9 @@ function addRowStatement() {
     
     select.setAttribute("id", "products_" + generate_num.toString())
     div.appendChild(select)
-    div.innerHTML = div.innerHTML + '<input type="number" name="quantity_1" id="quantity_' + generate_num.toString() + '" />\
-      <input type="number" name="price_1" id="price_' + generate_num.toString() + '" />\
-      <input type="number" name="pv_1" id="pv_' + generate_num.toString() + '" />\
+    div.innerHTML = div.innerHTML + '<input type="number" name="quantity_1" id="quantity_' + generate_num.toString() + '" value="0" />\
+      <input type="number" name="price_1" id="price_' + generate_num.toString() + '" value="0" />\
+      <input type="number" name="pv_1" id="pv_' + generate_num.toString() + '" value="0" />\
       <button name="button" type="button" onclick="javascript:deleteRow(' + generate_num.toString() + ')">delete</button>';
     //console.log(div.innerHTML)
     document.getElementById('productlist').appendChild(div);
@@ -74,4 +74,37 @@ function deleteRow(row_num) {
     var parentNode = delete_target.parentNode
     parentNode.removeChild(delete_target)
     console.log(row_num)
+    calculatePrice()
+}
+
+function calculatePrice() {
+    var productelement = document.getElementById('productlist').children
+    var totalprice = 0
+    var totalpv = 0
+    for (i = 0; i < productelement.length; i++) {
+        rowelement = productelement[i].children
+        console.log(rowelement.length)
+        
+        for (j = 2; j < rowelement.length; j++) {
+            if(rowelement[j].id.split("_")[0] == "price") {
+                totalprice += parseInt(rowelement[j].value)
+            }
+            else if (rowelement[j].id.split("_")[0] == "pv"){
+                totalpv += parseInt(rowelement[j].value)
+            }
+            console.log(rowelement[j])
+        }
+    }
+    document.getElementById('order_total_price').value = totalprice
+    document.getElementById('order_total_pv').value = totalpv
+}
+
+function changeSelection(select) {
+    var parent = select.parentElement.children
+    console.log(parent)
+    console.log(select.value)
+    parent[1].value = 1 //quantity value
+    parent[2].value = select.value //price value
+    parent[3].value = 1 //pv value
+    calculatePrice()
 }
